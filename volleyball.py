5039400a16a3385a70be29737324f456e9599206
Patryk Napiórkowski <patryk.napiorkowski95@gmail.com>
2026-05-12 22:47:15 +0200

    added simple data for practice and split the data for ML model

--- volleyball.py
+++ volleyball.py
@@ -1,10 +1,27 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
-data = {
-    'akcja': ['Atak', 'Przyjęcie', 'Błąd'],
-    'skutecznosc': [55, 60, 4]
-}
+# data = {
+#     'akcja': ['Atak', 'Przyjęcie', 'Błąd'],
+#     'skutecznosc': [55, 60, 4]
+# }
 
-df = pd.DataFrame(data)
-print("Twoja pierwsza tabela w Pythonie:")
-print(df)
+# df = pd.DataFrame(data)
+# print("Twoja pierwsza tabela w Pythonie:")
+# print(df)
+
+df = pd.read_csv('mecze.csv')
+
+srednie_bledy = df['bledy'].mean()
+print(f"Średnia liczba błędów w bazie: {srednie_bledy}")
+
+# Cechy (wszystko poza wynikiem i nazwą drużyny)
+X = df[['atak_skutecznosc', 'przyjecie_skutecznosc', 'bledy']]
+
+# Cel (tylko kolumna wynik)
+y = df['wynik']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+print(f"Liczba meczów do nauki: {len(X_train)}")
+print(f"Liczba meczów do testu: {len(X_test)}")
